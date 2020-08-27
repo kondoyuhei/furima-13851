8b5399e32d2ac19f05623fe70f191b48765637b5
kondoyuhei <gyomu@kondo-yuhei.com>
2020-08-27 21:51:34 +0900

    バリデーションの記述の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,26 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   with_options presence: true do
+    # 正規表現の条件指定
+    format_zenkaku  = /\A[ぁ-んァ-ン一-龥]/ # 全角（全角ひらがな・全角カタカナ・漢字）
+    format_katakana = /\A[ァ-ン]/ # 全角カタカナ
+
+    # エラーメッセージ
+    message_zenkaku = "is invalid. Input full-width characters." # 全角で入力させる
+    message_katakana = "is invalid. Input full-width Katakanas." # 全角カタカナで入力させる
+
     # ニックネーム
     validates :nickname
-    # Eメール
-    validates :email, format: { with: /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/, message: "is invalid. Email must include '@'. " }
     # パスワード
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid." }
     # 姓
-    validates :name_sei, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+    validates :name_sei, format: { with: format_zenkaku, message: message_zenkaku }
     # 名
-    validates :name_mei, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+    validates :name_mei, format: { with: format_zenkaku, message: message_zenkaku }
     # 姓読み
-    validates :yomi_sei, format: { with: /\A[ァ-ン]/, message: "is invalid. Input full-width Katakanas." }
+    validates :yomi_sei, format: { with: format_katakana, message: message_katakana }
     # 名読み
-    validates :yomi_mei, format: { with: /\A[ァ-ン]/, message: "is invalid. Input full-width Katakanas." }
+    validates :yomi_mei, format: { with: format_katakana, message: message_katakana }
     # 生年月日
     validates :birthday
   end
