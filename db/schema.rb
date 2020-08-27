55c271a637a32e131d6a9c2e5570ac247933095e
kondoyuhei <gyomu@kondo-yuhei.com>
2020-08-28 00:04:09 +0900

    Itemモデル・itemsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_144807) do
+ActiveRecord::Schema.define(version: 2020_08_27_145431) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,21 @@ ActiveRecord::Schema.define(version: 2020_08_27_144807) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "note", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
+    t.integer "category", null: false
+    t.integer "condition", null: false
+    t.integer "charge", null: false
+    t.integer "from", null: false
+    t.integer "period", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "name_sei", null: false
@@ -52,4 +67,5 @@ ActiveRecord::Schema.define(version: 2020_08_27_144807) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
