82d12704e4db7771e923f7759807b3a2d6ed816d
kondoyuhei <gyomu@kondo-yuhei.com>
2020-08-28 11:19:48 +0900

    Periodモデルの作成

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -3,6 +3,6 @@ class Charge < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '購入者負担（着払い）' },
-    { id: 3, name: '出品者負担（送料込み）' },
+    { id: 3, name: '出品者負担（送料込み）' }
   ]
 end
