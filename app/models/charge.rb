f023401f3acf457afba18ec16ecf25271107ff18
kondoyuhei <gyomu@kondo-yuhei.com>
2020-08-28 11:34:03 +0900

    ActiveHash使用モデルのブランク削除

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,8 +1,7 @@
 class Charge < ActiveHash::Base
   # 商品の送料負担
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '購入者負担（着払い）' },
-    { id: 3, name: '出品者負担（送料込み）' }
+    { id: 1, name: '購入者負担（着払い）' },
+    { id: 2, name: '出品者負担（送料込み）' }
   ]
 end
