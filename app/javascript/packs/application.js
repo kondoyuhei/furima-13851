b83cf6bec04d530e02bb6e9351fdd13a9c08a41c
kondoyuhei <gyomu@kondo-yuhei.com>
2020-08-29 16:40:39 +0900

    販売手数料・販売利益計算機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../calculation") // 販売手数料・販売利益の計算
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
