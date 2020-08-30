91ff7d04d89b9b7f7f228ba72ef84795c7bfe2e8
kondoyuhei <gyomu@kondo-yuhei.com>
2020-08-30 14:14:28 +0900

    出品ページ・商品ダミー生成のコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     price { rand(300..9_999_999) }
     category { rand(1..10) }
     condition { rand(1..6) }
-    charge { rand(1..4) }
+    charge { rand(1..2) }
     from { rand(1..47) }
     period { rand(1..3) }
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/files/test.png'), 'image/png') }
