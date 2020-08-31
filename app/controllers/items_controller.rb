7e365579f054e1ab6a71b6a3c9c4f956ccd9ff0f
kondoyuhei <gyomu@kondo-yuhei.com>
2020-08-31 13:19:53 +0900

    itemsコントローラーの共通処理のまとめ・コメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   # ログイン確認をする
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
   before_action :confirm_user, only: [:edit, :update, :destroy]
 
   def confirm_user
@@ -11,12 +12,15 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
   end
 
   def show
-    @item = Item.includes(:user).find(params[:id])
   end
 
   def new
@@ -34,11 +38,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       flash[:notice] = "商品の情報を更新しました"
       redirect_to item_path(@item.id)
