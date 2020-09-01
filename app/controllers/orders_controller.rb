88e46e77604de275cf829730446971432929c1c8
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-01 20:27:34 +0900

    ordersコントローラー・購入ページのフォームの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < ApplicationController
   require 'active_support'
   before_action :authenticate_user!
+  before_action :set_item
   before_action :item_available, only: [:index, :create]
   before_action :prohibit_buy_back, only: [:index, :create]
 
-  def item_available
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def item_available
     return if @item.on_sale
 
     # 売り切れであればトップページに遷移する
@@ -14,7 +18,6 @@ class OrdersController < ApplicationController
   end
 
   def prohibit_buy_back
-    @item = Item.find(params[:item_id])
     return unless @item.owner(current_user)
 
     # 出品者が自分の商品の購入URLにアクセスしたらトップページに遷移する
@@ -23,28 +26,50 @@ class OrdersController < ApplicationController
   end
 
   def index
-    @order = PurchaseShipping.new
+    @purchase_shipping = PurchaseShipping.new
+    # @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
-    purchase = Purchase.new(
-      user_id: purchase_params[:user_id],
-      item_id: purchase_params[:item_id]
-    )
-    if purchase.save
-      order_params = purchase_params.except(:token, :price)
+    @purchase_shipping = PurchaseShipping.new(purchase_params.except(:token, :price))
+    if @purchase_shipping.valid?
+      # 購入情報の保存
+      @purchase_shipping.save
+      # pay.jpに売上情報を送る
       pay_item
-      # @order.save
-      flash[:notice] = "商品を購入しました"
+      # 成功メッセージとともにトップページに遷移
+      flash[:notice] = "カード決済と購入が完了しました。"
       redirect_to root_path
+    else
+      render 'orders/index'
     end
 
+    # ********************うまくできたコード********************
+    # purchase = Purchase.new(
+    #   user_id: purchase_params[:user_id],
+    #   item_id: purchase_params[:item_id]
+    # )
+    # order_params = purchase_params.except(:token, :price, :user_id)
+    # @shipping = Shipping.new(order_params)
+
+    # if shipping.save 
+    #   # 購入情報を保存する
+    #   purchase.save
+    #   # pay.jpに売上情報を送る
+    #   pay_item
+    #   # 成功メッセージとともにトップページに遷移
+    #   flash[:notice] = "カード決済と購入が完了しました。"
+    #   redirect_to root_path
+    # else
+    #   flash[:notice] = "カード決済と購入はまだ済んでいません。"
+    #
+    # end
   end
 
   private
 
   def purchase_params
+    # 購入するアイテム・購入するユーザー・送り先の情報・カード決済トークンを含むストロングパラメーターの設定
     params.permit(
       :token, :purchase_id, :zip, :prefecture, :city,
       :address, :building, :phone
@@ -54,9 +79,9 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = "sk_test_39407efff624d99a308f69de"  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: purchase_params[:price],           # 商品の値段
-      card: purchase_params[:token], # カードトークン
-      currency:'jpy'                 # 通貨の種類(日本円)
+      amount: purchase_params[:price], # 商品の値段
+      card:   purchase_params[:token], # カードトークン
+      currency:'jpy'                   # 通貨の種類(日本円)
     )
   end
 end
