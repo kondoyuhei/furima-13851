11340b0a9dbdc191617e0977afd0f32d8126cfd2
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-02 15:37:57 +0900

    購入・送り先情報のテストコードの作成

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Shipping, type: :model do
+  describe '#create' do
+    before do
+      @purchase = FactoryBot.build(:purchase)
+    end
+
+    it "「購入id」「ユーザーid」が存在すれば登録できる" do
+      expect(@purchase).to be_valid
+    end
+
+    it "「商品id」が空では登録できない" do
+      @purchase.item_id = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("Item can't be blank")
+    end
+
+    it "「ユーザーid」が空では登録できない" do
+      @purchase.user_id = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("User can't be blank")
+    end
+  end
 end
