b32505e09a01ce9f5fd6d6c233364d966f84e2d7
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-02 16:11:42 +0900

    コードの記述法の修正

--- spec/factories/shippings.rb
+++ spec/factories/shippings.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :shipping do
     purchase_id { rand(1..1000) } # 購入id
-    zip        { rand(1..999).to_s.rjust(3, '0') + "-" + rand(1..9999).to_s.rjust(4, '0') } # 郵便番号
+    zip { rand(1..999).to_s.rjust(3, '0') + "-" + rand(1..9999).to_s.rjust(4, '0') } # 郵便番号
     prefecture { rand(1..47) } # 都道府県
-    city       { Faker::Address.city } # 市区町村
-    address   { Faker::Address.street_address } # 番地
-    phone      { "0" + rand(1..999_999_999).to_s.center(9, '0') } # 電話番号
+    city { Faker::Address.city } # 市区町村
+    address { Faker::Address.street_address } # 番地
+    phone { "0" + rand(1..999_999_999).to_s.center(9, '0') } # 電話番号
   end
 end
