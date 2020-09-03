7de9e236e6c90e489e30dbe90e953cc7708ed85b
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-03 12:32:53 +0900

    ordersコントローラーの修正
    before_actionで実行するメソッドをprivateに移動

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,26 +5,6 @@ class OrdersController < ApplicationController
   before_action :item_available, only: [:index, :create]
   before_action :prohibit_buy_back, only: [:index, :create]
 
-  def set_item
-    @item = Item.find(params[:item_id])
-  end
-
-  def item_available
-    return if @item.on_sale
-
-    # 売り切れであればトップページに遷移する
-    flash[:notice] = "アクセスした商品は売り切れです。"
-    redirect_to root_path
-  end
-
-  def prohibit_buy_back
-    return unless @item.owner(current_user)
-
-    # 出品者が自分の商品の購入URLにアクセスしたらトップページに遷移する
-    flash[:notice] = "出品者が自分の出品した商品を購入することはできません。"
-    redirect_to root_path
-  end
-
   def index
     # バリデーションできるよう送り先情報にダミーの購入id（purchase_id）に「0」を代入する。
     # 購入の確定前は購入idが未定のため、送り先情報（@shipping）のバリデーションができない。
@@ -35,7 +15,6 @@ class OrdersController < ApplicationController
     # 送り先情報の保存には不要なパラメーターを削除する
     shipping_params = purchase_params.except(:price, :user_id, :item_id)
     @shipping = Shipping.new(shipping_params.merge(purchase_id: 0))
-    binding.pry
     # 送り先のバリデーションが通れば購入を確定させる。
     if @shipping.valid?
       # 購入情報を保存する
@@ -58,6 +37,26 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def item_available
+    return if @item.on_sale
+
+    # 売り切れであればトップページに遷移する
+    flash[:notice] = "アクセスした商品は売り切れです。"
+    redirect_to root_path
+  end
+
+  def prohibit_buy_back
+    return unless @item.owner(current_user)
+
+    # 出品者が自分の商品の購入URLにアクセスしたらトップページに遷移する
+    flash[:notice] = "出品者が自分の出品した商品を購入することはできません。"
+    redirect_to root_path
+  end
 
   def purchase_params
     # 購入するアイテム・購入するユーザー・送り先の情報・カード決済トークンを含むストロングパラメーターの設定
