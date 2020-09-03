0ec49f00713e8566d4c0d343c7c10e54b321ccc9
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-03 11:19:57 +0900

    購入時のトークンのバリデーション設定

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,12 +1,13 @@
 class Shipping < ApplicationRecord
+  attr_accessor :token
   with_options presence: true do
     # 正規表現の条件指定
-    format_zip = /\A[0-9]{3}-[0-9]{4}\z/
-    format_phone = /\A[0-9]{10,11}\z/
+    format_zip = /\A[0-9]{3}-[0-9]{4}\z/ # 郵便番号
+    format_phone = /\A[0-9]{10,11}\z/ # 電話番号
     # バリデーション設定
     validates :purchase_id # 購入id
     validates :zip, format: {
-      with: zip_code,
+      with: format_zip,
       message: "is invalid. Include hyphen(-)"
     }                      # 郵便番号
     validates :prefecture, numericality: {
@@ -15,8 +16,9 @@ class Shipping < ApplicationRecord
     validates :city        # 市区町村
     validates :address     # 住所
     validates :phone, format: {
-      with: /\A[0-9]{10,11}\z/,
+      with: format_phone,
       message: "is invalid. Phone should be 10 or 11 digits long."
     }                      # 電話番号
+    validates :token # トークン
   end
 end
